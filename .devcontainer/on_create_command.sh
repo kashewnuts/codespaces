0964a5754eba51a2f65d3f25b2b70c3af7678b31
kashewnuts <kashun.nuts@gmail.com>
2022-06-07 00:31:36 +0900

    update setup sample

--- .devcontainer/on_create_command.sh
+++ .devcontainer/on_create_command.sh
@@ -2,16 +2,15 @@
 set -ex
 
 # to use docker-compose command
-cp -n .env.tmp .env || true
+cp -n .env.sample .env || true
 
 # initial setup docker components
 export BUILDKIT_PROGRESS=plain
-# docker login --username $DOCKER_USERNAME -p $DOCKER_LOGIN_PASS
 docker-compose up --build --no-start
 docker-compose stop  # prebuildのため落としておきたい
 
 # install python packages for local development
-(cd backend && pip install -r requirements.txt --no-warn-script-location)
+(cd backend && pip install -r requirements.lock --no-warn-script-location)
 
 # install node.js packages for local development
 (cd frontend && npm install)
